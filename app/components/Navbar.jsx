1c29af5073c2ed2aedef698dc99285ec645dcdc9
NishthaSharma-22 <nishthapinjore@gmail.com>
2025-06-19 21:44:41 +0530

    added navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <ul className="nav-links">
+                <li><a href="#">about</a></li>
+                <li><a href="#">projects</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </nav>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <main>
-     
+      <Navbar/>
+      <Hero/>
      
 
      
     </main>
   );
 }
+
