b5b78b451b226c861827a54fde02cfdeafe087b7
NishthaSharma-22 <nishthapinjore@gmail.com>
2025-06-22 19:53:51 +0530

    fixed potential case errors

--- app/components/Desktop.jsx
+++ app/components/Desktop.jsx
@@ -2,7 +2,7 @@
 import IntroCard from "./IntroCard";
 import { useState } from "react";
 import Linkedin from "./iconList/Linkedin";
-import Photos from "./iconList/photos";
+import Photos from "./iconList/Photos";
 
 export default function Desktop() {
   const [showWelcome, setShowWelcome] = useState(true);
