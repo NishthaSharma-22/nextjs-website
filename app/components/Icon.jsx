2a5f65b5145cc337b1b59518cf86387fd5ae0a71
NishthaSharma-22 <nishthapinjore@gmail.com>
2025-06-22 15:19:24 +0530

    added icon component

---
+++ app/components/Icon.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Image from "next/image";
+import styles from "../styles/Icon.module.css";
+
+export default function Icon({ iconSrc, label, onClick }) {
+  return (
+    <div className={styles.iconI} onClick={onClick}>
+      <Image src={iconSrc} alt={label} width={100} height={100} />
+      <p>{label}</p>
+    </div>
+  );
+}
