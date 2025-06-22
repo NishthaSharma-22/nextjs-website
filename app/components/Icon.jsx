45541b72b7f633470b8a0b297e88582f30666439
NishthaSharma-22 <nishthapinjore@gmail.com>
2025-06-22 17:24:41 +0530

    fix for build issue

--- app/components/Icon.jsx
+++ app/components/Icon.jsx
@@ -5,7 +5,9 @@ import styles from "../styles/Icon.module.css";
 export default function Icon({ iconSrc, label, onClick }) {
   return (
     <div className={styles.iconI} onClick={onClick}>
-      <Image src={iconSrc} alt={label} width={100} height={100} />
+      {iconSrc ? (
+        <Image src={iconSrc} alt={label} width={100} height={100} />
+      ) : null}
       <p>{label}</p>
     </div>
   );
