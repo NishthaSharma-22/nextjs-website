8201363c80495a26a8ce18cf2c84db40d323df80
Nishtha Sharma <nishthapinjore@gmail.com>
2025-08-15 16:21:07 +0530

    updated project icon content and styling

--- app/components/iconList/Projects.jsx
+++ app/components/iconList/Projects.jsx
@@ -1,4 +1,6 @@
 import AppIconWithWindow from "../AppIconWithWindow";
+import projects from "@/app/data/projects";
+import styles from "../../styles/Projects.module.css";
 
 export default function Project() {
   return (
@@ -6,7 +8,23 @@ export default function Project() {
       iconSrc="/email.png"
       label="Projects"
       windowTitle="Projects"
-      windowContent={<></>}
+      windowContent={
+        <div className={styles.projsGrid}>
+          {projects.map((project) => (
+            <div key={project.id} className={styles.projectCard}>
+              <img
+                src={project.icon}
+                alt={project.name}
+                className={styles.projIcon}
+              />
+              <a href={project.url} className={styles.link}>
+                <h3>{project.name}</h3>
+              </a>
+              <p>{project.description}</p>
+            </div>
+          ))}
+        </div>
+      }
     />
   );
 }
---
+++ app/data/projects.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    id: 1,
+    name: "Banana Jump",
+    icon: "",
+    url: "",
+    description:
+      "A platformer where you collect as many bananas as possible while trying to avoid the hard-to-spot rocks.",
+  },
+  {
+    id: 2,
+    name: "voyagerpad",
+    icon: "/projects/voyagerpad.png",
+    url: "https://github.com/NishthaSharma-22/voyager-pad",
+    description:
+      "A custom 7-key macropad with a 128×32 OLED display, PCB, and case, with open-source files for anyone to build their own",
+  },
+];
+
+export default projects;
