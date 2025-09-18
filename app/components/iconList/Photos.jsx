49ff29f9c12bdc62c3109117ce7bc2a0edb34f9f
Nishtha Sharma <nishthapinjore@gmail.com>
2025-09-19 00:57:48 +0530

    added data and styling for photos window

--- app/components/iconList/Photos.jsx
+++ app/components/iconList/Photos.jsx
@@ -1,6 +1,7 @@
 import AppIconWithWindow from "../AppIconWithWindow";
 import styles from "../../styles/Photos.module.css";
 import { IoMdPhotos } from "react-icons/io";
+import { photos } from "@/app/data/photos";
 
 export default function Photos() {
   return (
@@ -8,7 +9,16 @@ export default function Photos() {
       iconSrc={<IoMdPhotos size={64} color="#e5e7e8ff" />}
       label="Photos"
       windowTitle="Photos"
-      windowContent={<></>}
+      windowContent={
+        <div className={styles.photosContainer}>
+          {photos.map((photo, idx) => (
+            <div key={idx} className={styles.galleryItem}>
+              <img src={photo.src} alt={photo.caption} />
+              <div className={styles.overlay}>{photo.caption}</div>
+            </div>
+          ))}
+        </div>
+      }
     />
   );
 }
---
+++ app/data/photos.js
@@ -0,0 +1,12 @@
+export const photos = [
+  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+];
