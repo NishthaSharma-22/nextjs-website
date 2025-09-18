0f89d2bb5df7426c8e4fcf39d6c346680f0a431b
Nishtha Sharma <nishthapinjore@gmail.com>
2025-09-18 09:34:31 +0530

    added projects list and updated projects window

--- app/components/iconList/Projects.jsx
+++ app/components/iconList/Projects.jsx
@@ -2,6 +2,7 @@ import AppIconWithWindow from "../AppIconWithWindow";
 import projects from "@/app/data/projects";
 import styles from "../../styles/Projects.module.css";
 import { VscFileCode } from "react-icons/vsc";
+import { FaGithub, FaLink } from "react-icons/fa6";
 
 export default function Project() {
   return (
@@ -18,9 +19,30 @@ export default function Project() {
                 alt={project.name}
                 className={styles.projIcon}
               />
-              <a href={project.url} className={styles.link}>
-                <h3>{project.name}</h3>
-              </a>
+              <div className={styles.projName}>
+                {project.name}
+                <div className={styles.icons}>
+                  {project.github && (
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaGithub size={20} />
+                    </a>
+                  )}
+                  {project.url && (
+                    <a
+                      href={project.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLink size={20} />
+                    </a>
+                  )}
+                </div>
+              </div>
+
               <p>{project.description}</p>
               <div className={styles.tags}>
                 {project.tags?.map((tag, i) => (
--- app/data/projects.js
+++ app/data/projects.js
@@ -3,7 +3,8 @@ const projects = [
     id: 1,
     name: "Banana Jump",
     icon: "/projects/banana_jump.png",
-    url: "https://github.com/NishthaSharma-22/voyager-pad",
+    github: "https://github.com/NishthaSharma-22/banana-jam",
+    url: "https://nishthasharma-22.itch.io/banana-jam",
     description:
       "A platformer where you collect as many bananas as possible while trying to avoid the hard-to-spot rocks.",
     tags: ["Godot", "GDScript"],
@@ -12,7 +13,8 @@ const projects = [
     id: 2,
     name: "voyagerpad",
     icon: "/projects/voyagerpad.png",
-    url: "https://github.com/NishthaSharma-22/voyager-pad",
+    github: "https://github.com/NishthaSharma-22/voyager-pad",
+    url: "",
     description:
       "A custom 7-key macropad with a 128×32 OLED display, PCB, and case, with open-source files for anyone to build their own macropad.",
     tags: ["KiCad", "Python", "Fusion 360"],
@@ -21,7 +23,8 @@ const projects = [
     id: 3,
     name: "Khan SAT Math Toolbar",
     icon: "/projects/Khan_sat_toolbar.png",
-    url: "",
+    github: "https://github.com/NishthaSharma-22/khan-academy-extension",
+    url: "https://chromewebstore.google.com/detail/hkflbpkbhdlhikfbophoenepogkpklnf?utm_source=item-share-cb",
     description:
       "A browser extension to add desmos and SAT refernce sheet to Khan Academy website for students to have the same setup as the actual SAT.",
     tags: ["JavaScript"],
@@ -30,6 +33,7 @@ const projects = [
     id: 4,
     name: "Star ChatGPT Chats",
     icon: "/projects/chatgpt_star.png",
+    github: "https://github.com/NishthaSharma-22/star-gpt-chats",
     url: "",
     description:
       "A browser extension that lets you star/unstar ChatGPT chats - allows you to find important chats real quick!",
@@ -39,7 +43,8 @@ const projects = [
     id: 5,
     name: "FitElderAI",
     icon: "/projects/Fit_elder_ai.png",
-    url: "",
+    github: "https://github.com/NishthaSharma-22/FitElderAI",
+    url: "https://fitelderaigit-xjsxn9egktixtqbytdsjzz.streamlit.app/",
     description:
       "AI-driven upper body fitness app for seniors with Rikli and Jones flexibility tests using computer vision.",
     tags: ["Python", "Computer Vision"],
@@ -48,9 +53,20 @@ const projects = [
     id: 6,
     name: "NishthaOS",
     icon: "/projects/WebOS.png",
-    url: "",
+    github: "https://github.com/NishthaSharma-22/nextjs-website",
+    url: "https://nextjs-website-xone-git-main-nishthasharma22s-projects.vercel.app/",
     description: "My personal website as a webOS!",
     tags: ["NextJS", "ReactJS"],
+  },
+  {
+    id: 7,
+    name: "XIAO-Timer",
+    icon: "/projects/xiao-timer.png",
+    github: "https://github.com/NishthaSharma-22/xiao-timer",
+    url: "",
+    description:
+      "A compact, custom-built PCB timer designed for precise time tracking.",
+    tags: ["C++", "KiCad"],
   },
 ];
 
