eade6567fa4f85b978bd2cc3f504f8584135f374
Nishtha Sharma <nishthapinjore@gmail.com>
2025-09-19 10:46:28 +0530

    fixed photos window content styling

--- app/data/photos.js
+++ app/data/photos.js
@@ -1,12 +1,21 @@
 export const photos = [
   { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
-  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
-  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
-  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
-  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
-  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
-  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
-  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
-  { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
-  { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+  {
+    src: "/images/voyagerpad.png",
+    caption: "Case made in Fusion 360",
+  },
+  // { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  // {
+  //   src: "/images/voyagerpad.png",
+  //   caption: "SAT Math toolbar chrome extension",
+  // },
+  // { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  // { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
+  // { src: "/images/pcb.jpg", caption: "PCB for Macropad" },
+  // {
+  //   src: "/images/voyagerpad.png",
+  //   caption: "SAT Math toolbar chrome extension",
+  // },
+  // { src: "/images/voyagerpad.png", caption: "PCB for Macropad" },
+  // { src: "/images/pcb.jpg", caption: "SAT Math toolbar chrome extension" },
 ];
